cad6e4b78e7d3a7b03f2d15083a65a8e9fdac0ad
Przemyslaw <przemoacun@gmail.com>
2020-12-03 10:49:11 +0100

    second commit, Task.java and TaskRepository.java created

---
+++ src/main/java/pl/pm/todoapp/model/TaskRepository.java
@@ -0,0 +1,9 @@
+    package pl.pm.todoapp.model;
+
+    import org.springframework.data.jpa.repository.JpaRepository;
+    import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+    @RepositoryRestResource
+    interface TaskRepository extends JpaRepository<Task, Integer> {
+
+    }
